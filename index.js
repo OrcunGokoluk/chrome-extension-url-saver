7e3ef3ea5198c3803df368124a450b99877b2d1f
OrcunGokoluk <orcun.gokoluk@outlook.com>
2024-12-11 22:00:12 +0300

    added-basic-script-and-icon

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+let myLeads=[];
+
 
 const inputEl =document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn"); 
+const urlContainer=document.getElementById("url-container")
+
+
+inputBtn.addEventListener("click", function(){
+    const text=String(inputEl.value);
+    const trimmed=text.trim();
+    if(trimmed!=""&&!trimmed.includes(" "))
+    {
+        myLeads.push(inputEl.value)
+        inputEl.value = ""
+        renderLeads()
+    } 
+})
+
+function renderLeads() {
+    
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // Wrap the lead in an anchor tag (<a>) inside the <li>
+        // Can you make the link open in a new tab?
+        listItems += `<li><a href="https://${myLeads[i]}" target="_blank">${myLeads[i]} </a></li>`
+    }
+    urlContainer.innerHTML = listItems  
+}
